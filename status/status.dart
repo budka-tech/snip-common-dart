900a513d30bac0ef933c67551332ee0326a3b1ea
Pa Khan <don@pa-khan.com>
2024-08-27 17:01:58 +0300

    init

---
+++ status/status.dart
@@ -0,0 +1,49 @@
+enum Status {
+  /** Успешно */
+  Success,
+
+  /** Любая ошибка */
+  Any,
+
+  /** Не найдено */
+  NotFound,
+
+  /** Недостаточно прав */
+  PermissionDenied,
+
+  /** Слишком частые запросы */
+  TooFrequentRequests,
+
+  /** Внутренняя ошибка сервера */
+  InternalError,
+
+  /** Много подключений */
+  ManyConnections,
+
+  /** Недостаточно аргументов */
+  NotEnoughArguments,
+
+  /** Не авторизован */
+  NotAuthorized,
+
+  /** Уже авторизован */
+  Authorized,
+
+  /** Неверное значение */
+  IncorrectValue,
+
+  /** Бездействие */
+  Inactivity,
+
+  /** Время ожидания истекло */
+  Timeout,
+
+  /** Неверный ввод */
+  InvalidInput,
+
+  /** Ресурс недоступен */
+  ResourceUnavailable,
+
+  /** Операция не удалась */
+  OperationFailed
+}
